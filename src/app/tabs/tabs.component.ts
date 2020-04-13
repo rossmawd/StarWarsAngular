cf3d4232cca20e49db4d1035b8e709428f8cccb3
Ross Mawdsley <rossmawd@gmail.com>
2020-04-13 18:29:56 +0100

    connected chosenList

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -10,9 +10,24 @@ export class TabsComponent implements OnInit {
     {name: 'Luke Skywalker', side: ''},
     {name: 'Darth Vader', side: ''}
   ]
+  chosenList = 'all' //stores which side the user has clicked
   constructor() { }
 
   ngOnInit() {
   }
+
+  onChoose(side) {
+    this.chosenList = side
+  }
+
+  getCharacters = () : any[] => {
+    if (this.chosenList === 'all') {
+      return this.characters.slice() //returns a copy of an array
+    }
+    console.log('getting chars')
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList
+    })
+}
 
 }
